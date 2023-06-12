ad5aa31ca589f0b01d58f1576e762c66c801ce26
gosapp <gosapps86contact@gmail.com>
2023-06-12 14:50:48 -0300

    actualizando

---
+++ js/components/UserInfoComponent.js
@@ -0,0 +1,49 @@
+export const UserInfoComponent = (name, mail, description) =>
+
+`
+<div class="info__div" id="title__name">
+    <h2>¡Hola ${name}!</h2>
+</div>
+
+<div class="info__div">
+    <h3>Seguridad de la cuenta</h3>
+    <p>${mail}</p>
+    <button>Cambiar Contraseña</button>
+</div>
+
+<div class="info__div">
+    <h3>Sobre mi...</h3>
+    <input type="text" id="user__input" value="${description} ">
+</div>
+
+<div class="info__div">
+    <h3>Mi género</h3>
+    <input type="radio" id="female" name="gender">
+    <label for="female">Femenino</label>
+    <input type="radio" id="male" name="gender">
+    <label for="male">Masculino</label>
+    <input type="radio" id="others" name="gender">
+    <label for="others">Otros</label>
+</div>
+
+<div class="info__div">
+    <h3>Más sobre mi</h3>
+    <button>Ver/modificar</button>
+</div>
+
+<div class="info__div">
+    <h3>Que busco</h3>
+    <p>Edad mínima</p>
+    <input type="range" id="min__age" min="18" max="100">
+    <label for="min__age">18</label>
+    <p>Edad máxima</p>
+    <input type="range" id="max__age" min="18" max="120">
+    <label for="max__age">120</label>
+    <p>Distancia</p>
+    <input type="range" id="distance" min="0">
+    <label for="distance">100 km</label>
+    <p>Otros</p>
+    <button>Ver/modificar</button>
+</div>
+
+`
--- js/services/fetchAuthServices.js
+++ js/services/fetchAuthServices.js
@@ -1,17 +1,15 @@
-let urlBase = 'https://localhost:7017/api/v1/Auth/login';
+let urlBase = 'https://localhost:7017/api/v1/Auth';
 
 export const login = async (request) => {
 
     let result;
-    let response = await fetch(urlBase,{
+    let response = await fetch(`${urlBase}/login`,{
         method: "POST",
         headers:{
             "Content-Type": "application/json"
         },
         body: JSON.stringify(request)
     })
-    
-    console.log(response);
 
     if(response.ok && response.status == 200){
         result = await response.json();
@@ -24,4 +22,20 @@ export const login = async (request) => {
 }
 
 
-//localStorage.setItem("token", json.token);
+export const GetMail = async (request) => {
+
+    let result;
+    let response = await fetch(urlBase,{
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        }
+    })
+
+    if(response.ok){
+        result = await response.json();
+    }
+    
+    return result;
+}
