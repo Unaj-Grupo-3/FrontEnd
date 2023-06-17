70680247af3a8dcb199854e1466001bbe22b9fe5
hernan9011 <pipivr72@gmail.com>
2023-06-16 22:14:31 -0300

    defer

--- js/init/initChat.js
+++ js/init/initChat.js
@@ -3,10 +3,7 @@ import { addEventListenerChat } from "../container/chatPage.js";
 import { addEventListenerHub } from "../container/connectionHub.js"
 import { ObtenerChats } from "../services/fetchChatServices.js";
 
-
-document.addEventListener("DOMContentLoaded", async () => { 
-    localStorage.removeItem("currentChat");
-    await addEventListenerHub();
-    ObtenerChats(renderChats);
-    addEventListenerChat();
-});
+localStorage.removeItem("currentChat");
+await addEventListenerHub();
+ObtenerChats(renderChats);
+addEventListenerChat();
