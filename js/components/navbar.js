45e84d28ca20115c2c2432cb5b9a73ee28d3cc52
hernan9011 <pipivr72@gmail.com>
2023-06-21 01:21:52 -0300

    combio nav y agrego minihub

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,21 +1,16 @@
 export const Navbar = (userPhoto) =>
 `
-<header class="header" id="header">
-          <div class="header_toggle">
-            <i class="bi bi-list" id="header-toggle"></i>
-          </div>
-          <div class="header_img">
-            <img src="${userPhoto}" alt="" />
-          </div>
-        </header>
-        <div class="l-navbar" id="nav-bar">
-          <nav class="nav">
-            <div>
-              <a href="http://127.0.0.1:5501/views/homepage.html" class="nav_logo">
-                <img src="\\img\\globo-blanco.png" alt="Logo" class="logo-citas" />
-                <span class="nav_logo-name">CitasApp</span>
-              </a>
-              <div class="nav_list">
+    <div class="l-navbar" id="nav-bar">
+        <header  id="menu_toggle" class="header">            
+            <i class="bi bi-list"></i>
+            <span class="nav_name">logo</span>  
+        </header>           
+        <div id="header" class="header_user">
+            <img src="${userPhoto}" alt="" class="header_img" id="header_img"/>
+            <span class="header_name">nombre apellido</span>
+        </div>
+        <nav class="nav">
+            <div class="nav_list">
                 <a href="http://127.0.0.1:5501/views/UserPage.html" class="nav_link active">
                     <i class="bi bi-person-circle"></i>
                   <span class="nav_name">Perfil</span>
@@ -29,16 +24,14 @@ export const Navbar = (userPhoto) =>
                   <span class="nav_name">Citas</span>
                 </a>
                 <a href="../../views/Chat.html" class="nav_link">
-                    <i class="bi bi-chat-right-text"></i>
+                    <i id="nav-icono" class="bi bi-chat-right-text"></i>
                   <span class="nav_name">Chats</span>
                 </a>
-              </div>
+                <a href="#" class="nav_link">
+                    <i class="bi bi-box-arrow-left"></i>
+                    <span class="nav_name">Salir</span>
+                </a>
             </div>
-            <a href="#" class="nav_link">
-                <i class="bi bi-box-arrow-left"></i>
-              <span class="nav_name">Salir</span>
-            </a>
-          </nav>
-        </div>
-
+        </nav>
+    </div>
 `
