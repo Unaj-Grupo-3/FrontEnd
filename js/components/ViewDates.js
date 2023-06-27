f7ef67d9c79abce5a54ca4a54dffe3b0cd92eff3
Marcos N Sosa <markitos.sosa.nahuel@gmail.com>
2023-06-27 07:52:47 -0300

    Cambios en el codigo

--- js/components/ViewDates.js
+++ js/components/ViewDates.js
@@ -11,6 +11,53 @@ export const ProfileCard= (name, rootImage,id, idMatch) =>
 </div>
 `;
 
+const DatesCard = async (props) => {
+    const data = {
+        user: {
+            img: 'imagen.png',
+            name: 'Nombre' 
+        },
+        date: {
+            description: 'nombre de la cita',
+            date: 'fecha',
+            status: 'aceptada',
+            location: {
+                lat: '-34',
+                lng: '-53'
+            }
+        }
+    }
+    
+        return  `
+        <article class="dateDetail">
+            <div class="dateDetail__content">
+    
+                <div class="dateDetail_content_user">
+                    <img src=${data.user.img} alt=${data.user.name}>
+                    <h3>${data.user.name}</h3>
+                </div>
+                
+                <div class="dateDetail_content_detail">
+                    <h3>Lugar: ${data.date.description}</h3>
+                    <h2>Fecha: ${data.date.date}</h2>
+                    <h2>Hora: 20:00 h</h2>
+                </div>
+    
+                <div class="dateDetail_content_map">
+                </div>
+    
+            </div>
+    
+            <div class="dateDetail__button">
+                <button id="acceptDate">Aceptar</button>
+                <button id="cancelDate">Cancelar</button>
+            </div>
+        </article>
+        `
+    }
+    
+    export default DatesCard;
+
 function prueba(idMatch)
 {
     alert("Hola" + idMatch);
