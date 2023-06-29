0d63dad5752529cf84d4e42d1fa0cf14ffd0dee6
Franco051 <lopezfranco051@gmail.com>
2023-06-28 23:35:08 -0300

    PreferenceModifies

--- js/init/initPreferenceRegister.js
+++ js/init/initPreferenceRegister.js
@@ -4,7 +4,6 @@ import {PrefComponent, InterestTag} from '../components/UserInfoComponent.js';
 
 async function InterestOnClick(e){
 
-    console.log(this.id);
     let intIdString = this.id;
     let idx = intIdString.lastIndexOf("_") + 1;
     let intId = intIdString.slice(idx, intIdString.length);
@@ -25,27 +24,29 @@ async function InterestOnClick(e){
 
 async function RenderPref() {
 
-    const prefContainer = document.querySelector('.pref_container');
+    const prefContainers = document.querySelectorAll('.pref_container');
 
     let categories = await GetInterest();
 
-    categories.forEach((cat) =>
-    {
-        let ints4Cat = cat.interes;
-        let contName = '#cat_' + cat.id;
-
-        prefContainer.innerHTML += PrefComponent(cat.id, cat.description);
-        
-        let intContainer = document.querySelector(contName);
-
-        ints4Cat.forEach((item) =>
+    prefContainers.forEach((prefContainer, index) => {
+        categories.forEach((cat) =>
         {
-            intContainer.innerHTML += InterestTag(item.id, item.description);
-
-            let intId = "#my_int_" + item.id;
-        });
-    })
-
+            let ints4Cat = cat.interes;
+            let contName = '#cat_' + cat.id;
+    
+            prefContainer.innerHTML += PrefComponent(cat.id, cat.description);
+            
+            let intContainer = (document.querySelectorAll(contName))[index];
+    
+            ints4Cat.forEach((item) =>
+            {
+                intContainer.innerHTML += InterestTag(item.id, item.description);
+    
+                let intId = "#my_int_" + item.id;
+            });
+        })
+    
+    });
     
     let tagContainers = document.querySelectorAll('.interest_item');
     tagContainers.forEach((item) =>{
