1f68096a7c7a8bc20dea9fc76115db28963a7bc1
NicoHerlan <nicodvkcss@gmail.com>
2023-07-02 23:47:40 -0300

    fix cardDate

--- js/container/dates/filterDates.js
+++ js/container/dates/filterDates.js
@@ -1,27 +1,33 @@
 import { GetMyDates } from "../../services/fetchDatesServices.js"
 import DatesCard from "../../components/dates/DatesCard.js";
 import HeaderMyDates from "../../components/dates/HeaderMyDates.js";
+import { GetMyUser } from "../../services/fetchUserServices.js";
+import NoMatchsCard from "../../components/dates/NoMatchsCard.js";
+import NoDatesCard from "../../components/dates/NoDatesCard.js";
 
 export const filterDates = async (element) => {
     const sectionContainer = document.querySelector("#Container-Dates");
-
     let dates = await GetMyDates();
-    const result = [];
-    sectionContainer.innerHTML = HeaderMyDates();    
+    let arrayDates = dates.response;
+    let result = [];
+    sectionContainer.innerHTML = "";    
     if (dates.count > 0) {
         if (element.id != "") {
-            result = dates.filter(date => date.state == element.id);
+            result = arrayDates.filter(date => date.state == element.id);
         }
         else {
-            result = dates;
+            result = arrayDates;
         }
         
         if (result.length > 0) {
             for (let i = 0; i < result.length; i++) {
+                let userMe = await GetMyUser();
                 sectionContainer.innerHTML += await DatesCard(userMe, result[i]);
             }  
             
             console.log(dates);
+        }else{
+            sectionContainer.innerHTML = NoDatesCard();
         }
     }
 }
