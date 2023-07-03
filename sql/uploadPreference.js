913b75004778b354a3bf2fee227a1faa34562104
NicoHerlan <nicodvkcss@gmail.com>
2023-07-03 15:54:00 -0300

    add interest script

--- sql/uploadPreference.js
+++ sql/uploadPreference.js
@@ -1,37 +1,45 @@
 import { login } from "../js/services/fetchAuthServices.js";
-import { PostMyOverall, PostGenderPref } from "../js/services/fetchPreferenceServices.js" ;
+import { PostMyOverall, PostGenderPref, PostPreference } from "../js/services/fetchPreferenceServices.js" ;
 import { GetMyUser } from "../js/services/fetchUserServices.js";
-/*
+
 for(let i=1; i<101; i++) {
-    let data  = await login({
+    let data1  = await login({
         "email": `mail${i}@expresso.com`,
         "password": "Express0."
     });
 
-    sessionStorage.setItem("token", data.token);
+    sessionStorage.setItem("token", data1.token);
+    for(let j = 0; j < 20; j++) {
 
-    let user = await GetMyUser();
+        let typeInterest =  Math.floor(Math.random() * 3) + 1;;
+        let interestId = Math.floor(Math.random() * 82) + 1;
 
-    let preferenceRequest  = {
-            sinceAge: getEdad(user.birthday)-8 < 18?  18 :  getEdad(user.birthday)-8 ,
-            untilAge: getEdad(user.birthday) + 8,
-            distance: 15
+        let interestLoad = [];
+
+        while(interestLoad.includes(interestId)) {
+            interestId = Math.floor(Math.random() * 82) + 1;
+        }
+
+        let interestBody = {
+            interestId: interestId,
+            ownInterest: false,
+            like: true
+        }
+
+        if(typeInterest == 1){
+            interestBody.ownInterest = true;
+            interestBody.like = false ;
+        }else if(typeInterest == 2){
+            interestBody.ownInterest = true;
+            interestBody.like = true ;
+        }else if(typeInterest == 3){
+            interestBody.ownInterest = false;
+            interestBody.like = true ;
+        }
+
+        let data = await PostPreference(interestBody);
+        console.log(data);
     }
-    let genderBody = {
-        genderId : 0
-    }
-
-    await PostMyOverall(preferenceRequest);
-
-    if(user.gender.genderId == 1){
-        genderBody.genderId = 2
-    }else if(user.gender.genderId == 2){
-        genderBody.genderId = 1
-    }else{
-        genderBody.genderId = 3
-    }
-
-    await PostGenderPref(genderBody);
 }
 
 
@@ -45,4 +53,4 @@ function getEdad(birthday){
         }
 
         return edad;
-}*/
+}
