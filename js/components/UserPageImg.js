651069de9d084204ab67593d615fffb3689432c4
gosapp <gosapps86contact@gmail.com>
2024-02-10 10:43:06 -0300

    Clean code: se limpio y dividio el css de la pagina del usuario

--- js/components/UserPageImg.js
+++ js/components/UserPageImg.js
@@ -4,7 +4,7 @@ export const UserPageImg = (cont, id, img) =>
 <div class="drag__container user_photo_cont" id="drag_id_${cont}" draggable="true" ">
     <img src="${img}" class="drag__img" id="img_${id}" alt="Foto de usuario">
     <button class="btn_delete" value="${id}">
-        <span class="material-symbols-outlined icono-delete">
+        <span class="icono-delete material-symbols-outlined">
             delete
         </span>
     </button>
